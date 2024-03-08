26e6652e02a84766fe78b91e2eccbff60e39e631
sammy171207 <sameerrandive171207@gmail.com>
2024-03-08 20:25:55 +0530

    check the setup of react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+   <div>
+    <h1>Hello , this is first day of learning react</h1>
+    <button >Click here</button>
+   </div>
+  )
+}
+
+export default App
